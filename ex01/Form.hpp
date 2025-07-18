c4178d2bf07682821e8ec1a12d8c7b3d57a30f3c
Raain <ryanbwgt@gmail.com>
2025-07-18 23:51:52 +0200

    form hpp

---
+++ ex01/Form.hpp
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Form.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rmakoni <rmakoni@student.42heilbronn.de    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/18 23:38:46 by rmakoni           #+#    #+#             */
+/*   Updated: 2025/07/18 23:51:08 by rmakoni          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FORM_HPP
+#define FORM_HPP
+
+#include "Bureaucrat.hpp"
+
+class Form
+{
+    private:
+        const std::string name;
+        bool isSigned;
+        const int gradeToSign;
+        const int gradeToExecute;
+
+    public:
+        Form();
+        Form(const std::string &name, int gradeToSign, int gradeToExecute);
+        Form(const Form &other);
+        Form &operator=(const Form &other);
+        ~Form();
+
+        std::string getName() const;
+        bool getIsSigned() const;
+        int getGradeToSign() const;
+        int getGradeToExecute() const;
+
+        //exceptions
+        class GradeTooHighException : public std::exception
+        {
+            public:
+                GradeTooHighException();
+                ~GradeTooHighException() throw();
+                const char *what() const throw();
+        };
+
+        class GradeTooLowException : public std::exception
+        {
+            public:
+                GradeTooLowException();
+                ~GradeTooLowException() throw();
+                const char *what() const throw();
+        };
+
+        void beSigned(const Bureaucrat &bureaucrat);
+};
+
+#endif
